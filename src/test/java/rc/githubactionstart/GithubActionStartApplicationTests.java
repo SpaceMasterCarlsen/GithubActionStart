10482b13e16a916e9bfc81cd1f44b49472f14253
SpaceMasterCarlsen <96848056+SpaceMasterCarlsen@users.noreply.github.com>
2025-05-28 18:56:03 +0200

    Initial commit

---
+++ src/test/java/rc/githubactionstart/GithubActionStartApplicationTests.java
@@ -0,0 +1,13 @@
+package rc.githubactionstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
